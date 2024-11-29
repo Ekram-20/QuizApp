c4ae1145b40467858cf9633dfcf7061e45212cd5
Ekram Feras <ekramferasj@gmail.com>
2024-11-29 16:17:59 +0300

    feat: make timer for each question

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -4,10 +4,33 @@ import { useQuizContext } from "../providers/QuizProvider";
 import QuestionCard from "../components/QuestionCard";
 import Button from "../components/Button";
 import ResultsCard from "../components/ResultsCard";
+import { useEffect, useState } from "react";
 
 const QuizScreen = () => {
 
-  const { question, questionIndex, onNext, totalQuestions, isDone} = useQuizContext();
+  const { question, questionIndex, onNext, totalQuestions, isDone } = useQuizContext();
+  const [timer, setTimer] = useState(20);
+  
+  useEffect(() => {
+
+    setTimer(20);
+
+    // make function that decrease every 1s
+    const interval = setInterval(() => {
+      setTimer((t) => {
+        // decrease when it is in the time limited
+        if (t > 0)
+          return (t - 1)
+        // when the 20 is finish move to the next question
+        onNext();
+        return 0
+      })
+    }, 1000); // in seconds
+    
+    // clear previous question timer
+    return () => clearInterval(interval)
+
+  }, [question]); // make timer for each question when it change
 
   return (
     <SafeAreaView style={styles.page}>
@@ -21,7 +44,7 @@ const QuizScreen = () => {
         {question ? (
           <View>
             <QuestionCard question={question} />
-            <Text style={styles.time}>20 sec</Text>
+            <Text style={styles.time}>{timer} sec</Text>
           </View>
         ) : (
           <ResultsCard />
