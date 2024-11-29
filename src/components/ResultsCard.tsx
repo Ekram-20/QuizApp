392d8151d0d88f79dc1517ce97391eb8e8d6b004
Ekram Feras <ekramferasj@gmail.com>
2024-11-29 17:16:12 +0300

    feat: add animation at result

--- src/components/ResultsCard.tsx
+++ src/components/ResultsCard.tsx
@@ -1,18 +1,22 @@
 import { Text, StyleSheet } from 'react-native'
 import Card from './Card';
 import { useQuizContext } from '../providers/QuizProvider';
-
+import LottieView from 'lottie-react-native';
+import animationParty from '../../assets/party.json'
 
 const ResultsCard = () => {
 
   const {score, totalQuestions} = useQuizContext()
 
   return (
-    <Card>
-      <Text style={styles.title}>Done</Text>
-      <Text style={styles.body}>Correct Answers: {score}</Text>
-      <Text style={styles.body}>Best Score: {totalQuestions}</Text>
-    </Card>
+    <>
+      <LottieView autoPlay style={StyleSheet.absoluteFill} source={animationParty}/>
+      <Card>
+        <Text style={styles.title}>Done</Text>
+        <Text style={styles.body}>Correct Answers: {score}</Text>
+        <Text style={styles.body}>Best Score: {totalQuestions}</Text>
+      </Card>
+    </>
   )
 }
 
